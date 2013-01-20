d9350dd8841a6ae5bcffefff2abc48755e69e5b1
Anders Lindmark <anders.lindmark@gmail.com>
2013-01-21 00:54:41 +0100

    You can now "unwatch" videos that have been marked as watched

--- ythelper_userpage.js
+++ ythelper_userpage.js
@@ -5,8 +5,9 @@ defaultoptions = {
 };
 
 /*
-TODO: Deal with "Load more" (http://www.youtube.com/channel_ajax?action_load_more_videos)
-	A "not watched" button to undo videos that are marked as watched
+TODO:
+	- Deal with "Load more" (http://www.youtube.com/channel_ajax?action_load_more_videos)
+	- Move common stuff to a separate javascript-file and add that to the content_script chain
 */
 
 chrome.storage.local.get("ythelper_options", function (data)
@@ -20,7 +21,6 @@ chrome.storage.local.get("ythelper_options", function (data)
 	else {
 		options = data.ythelper_options;
 	}
-	console.log("Effects enabled: " + options.effect_enabled);
 	if (options.effect_enabled) {
 		var storage = null;
 		if (options.localstorage) {
@@ -38,11 +38,30 @@ chrome.storage.local.get("ythelper_options", function (data)
 			// Check every video box and see if it is in the history
 			$("span.context-data-item").each( function() {
 				// get video id and set parent item style.
-				var id = $(this).attr("data-context-item-id");
-				if (id && ids[id]) {
-					$(this).parent().css("opacity", options.opacity);
+				var video_id = $(this).attr("data-context-item-id");
+				if (video_id && ids[video_id]) {
+					//$(this).parent().css("opacity", options.opacity);
+					$(this).css("opacity", options.opacity);
+					$(this).find(".content-item-detail").css("opacity", options.opacity);
+					var offset = $(this).parent().offset();
+					var info = $(this).parent().prepend('<div class="thumbnail_overlay" id="' + video_id + '"">' + 
+						'<a href="#" class"thumbnail_overlay" id="' + video_id + '">Unwatch</a>' +
+						'</div>');
+					info.css("top", offset.top);
+					info.css("left", offset.left);
+					info.click( function(event) {
+						event.preventDefault();
+						unwatch(storage, ids, video_id);
+					});
 				}
 			});
 		});
 	}
 });
+
+function unwatch(storage, ids, video_id) {
+	delete ids[video_id];
+	storage.set({"watched_id": ids}, function() {
+		console.log("Unwatched " + video_id);
+	});
+}
